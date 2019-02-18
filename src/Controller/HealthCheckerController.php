0582ef2ffb1ccb0ab1945cfa2cb6346f42578cb3
Slavik <slavik.webdev@gmail.com>
2019-02-18 01:06:33 +0000

    ping method

---
+++ src/Controller/HealthCheckerController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\Get;
+
+/**
+ * Class HealthCheckerController
+ * @package App\Controller
+ */
+class HealthCheckerController extends FOSRestController
+{
+    /**
+     * @Get(path="ping")
+     * @return JsonResponse
+     */
+    public function getAction()
+    {
+        return new JsonResponse(
+            'pong'
+        );
+    }
+}
